9c4a2ce3a819487f06686dd76cf2b34f6a006aeb
Coursera Learner <nobody@nowhere.nul>
2019-01-11 15:03:15 +0000

    Added code

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -1,16 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int max(int x, int y)
+{
+  return x > y ? x : y;
+}
+
+int check_to_print_star(int size1, int x_offset, int y_offset, int size2, int x, int y)
+{
+  if ( ((x >= x_offset && x < (x_offset +size2)) && 
+	   (y==y_offset || y==(y_offset + size2 - 1)))
+           ||
+	   ((y >= y_offset && y < (y_offset + size2)) &&
+            (x==x_offset || x==(x_offset + size2 -1))) )
+    return 1;
+  else
+    return 0;
+}
+
+int check_to_print_pound(int size1, int x, int y)
+{
+  if ( ( (x < size1) && (y==0 || y==(size1-1)) )
+        ||
+       ( (y < size1) && (x==0 || x==(size1-1)))
+     )
+    return 1;
+  else
+    return 0;
+}
 
 void squares(int size1, int x_offset, int y_offset, int size2) {
   //compute the max of size1 and (x_offset + size2).  Call this w
-
+  int w=max(size1, x_offset + size2);
   //compute the max of size1 and (y_offset + size2).  Call this h
-
+  int h=max(size1, y_offset + size2);
+ 
   //count from 0 to h. Call the number you count with y
-
+  for (int y=0;y<h;y++){
     //count from 0 to w. Call the number you count with x
-
+    for (int x=0;x<w;x++){
       //check if  EITHER
       //    ((x is between x_offset  and x_offset +size2) AND 
       //     y is equal to either y_offset OR y_offset + size2 - 1 )
@@ -18,16 +46,22 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
       //    ((y is between y_offset and y_offset + size2) AND
       //     x is equal to either x_offset OR x_offset + size2 -1)
       // if so, print a *
-
+      if (check_to_print_star(size1, x_offset, y_offset, size2, x, y))
+	printf("*");
       //if not,
       // check if EITHER
       //    x is less than size1 AND (y is either 0 or size1-1)
       // OR
       //    y is less than size1 AND (x is either 0 or size1-1)
       //if so, print a #
-
+      else if (check_to_print_pound(size1, x, y))
+	printf("#");
       //else print a space
-    //when you finish counting x from 0 to w, 
+      else
+        printf(" ");
+    //when you finish counting x from 0 to w,
+    }
     //print a newline
-
+    printf("\n");
+  }
 }
