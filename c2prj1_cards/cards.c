20ad3a018142dfb65d74ff9512c763abefeae8cf
Coursera Learner <nobody@nowhere.nul>
2019-01-16 13:47:21 +0000

    code

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -5,34 +5,122 @@
 
 
 void assert_card_valid(card_t c) {
-
-
+  assert((c.value >=2 && c.value <= VALUE_ACE) && (c.suit >= SPADES && c.suit <= CLUBS));
 }
 
 const char * ranking_to_string(hand_ranking_t r) {
-  return "";
+    switch(r) {
+    case STRAIGHT_FLUSH: return "STRAIGHT_FLUSH";
+    case FOUR_OF_A_KIND: return "FOUR_OF_A_KIND";
+    case FULL_HOUSE: return "FULL_HOUSE";
+    case FLUSH: return "FLUSH";
+    case STRAIGHT: return "STRAIGHT";
+    case THREE_OF_A_KIND: return "THREE_OF_A_KIND";
+    case TWO_PAIR: return "TWO_PAIR";
+    case PAIR: return "PAIR";
+    default: return "NOTHING";
+      //    default:
+      //printf("Invalid ranking\n");
+      //return "Invalid ranking!";
+    }
 }
 
 char value_letter(card_t c) {
-  return 'x';
+  assert_card_valid(c);
+  if (c.value >= 2 && c.value <= 9)
+    return '0' + c.value;
+  else if (c.value == 10)
+    return '0';
+  else if (c.value == 11)
+    return 'J';
+  else if (c.value == 12)
+    return 'Q';
+  else if (c.value == 13)
+    return 'K';
+  else {
+    assert(c.value == 14);
+    return 'A';
+  }
 }
 
 
 char suit_letter(card_t c) {
-  return 'x';
-  
+  assert(c.suit >= SPADES && c.suit <= CLUBS);
+  switch(c.suit) {
+  case SPADES: return 's';
+  case HEARTS: return 'h';
+  case DIAMONDS: return 'd';
+  case CLUBS: return 'c';
+  case NUM_SUITS:
+    printf("Invalid card suit!\n");
+    exit(EXIT_FAILURE);
+  }
+  return 'c'; // dummy for build
 }
 
 void print_card(card_t c) {
-
+  printf("%c%c\n",value_letter(c), suit_letter(c));
 }
+
+ static unsigned value_from_letter(char value_let) {
+   if (value_let >= '2' && value_let <= '9')
+    return value_let - '0';
+  else if (value_let == '0')
+    return 10;
+  else if (value_let == 'J')
+    return VALUE_JACK;
+  else if (value_let == 'Q')
+    return VALUE_QUEEN;
+  else if (value_let == 'K')
+    return VALUE_KING;
+  else if (value_let == 'A')
+    return VALUE_ACE;
+  else {
+	printf("Invalid value letter!\n");
+	exit(EXIT_FAILURE);
+      }
+ }
+
+ static suit_t suit_from_letter(char suit_let)
+ {
+   if (suit_let == 's')
+     return SPADES;
+   else if (suit_let == 'h')
+     return HEARTS;
+   else if (suit_let == 'd')
+     return DIAMONDS;
+   else if (suit_let == 'c')
+     return CLUBS;
+   else
+     {
+       printf("Invalid suit letter!\n");
+       exit(EXIT_FAILURE);
+     }
+ }
 
 card_t card_from_letters(char value_let, char suit_let) {
   card_t temp;
+  temp.value = value_from_letter(value_let);
+  temp.suit = suit_from_letter(suit_let);
   return temp;
 }
 
+// h -> 0
+// d -> 1
+// c -> 2
+// s -> 3
+// That means, hearts: 0, 1 ...12
+// diamonds: 13, 14 ... 25
+// clubs: 26, 27... 38
+// spades: 39,40 ...51
+//
+// In other words:
+// c % 13 => c.value
+// c / 13 => c.suit
 card_t card_from_num(unsigned c) {
+  assert(c >= 0 && c < 52);
   card_t temp;
+  temp.value = c%13 + 2;
+  temp.suit = c/13; 
   return temp;
 }
--- c2prj1_cards/my-test-main.c
+++ c2prj1_cards/my-test-main.c
@@ -1,5 +1,58 @@
 #include "cards.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/*
+// e.g. str = "2s"
+static void check_card(unsigned c, const char* str)
+{
+  card_t card = card_from_num(c);
+  if (strcmp(card_from_num(c), str) != 0)
+    {
+      printf("Failed for card: %s \n", str);
+      exit(EXIT_FAILURE);
+    }
+
+  printf("Passed: %s \n", str);
+}
+*/
+void test_card_from_num()
+{
+  card_t c = card_from_num(0);
+  print_card(c); // 2s
+
+  c = card_from_num(1);
+  print_card(c); // 3s
+
+  c = card_from_num(12);
+  print_card(c); //As
+
+  c = card_from_num(13);
+  print_card(c); //2h
+
+  c = card_from_num(26);
+  print_card(c); //2d
+
+  c = card_from_num(38);
+  print_card(c); // Ad
+
+  c = card_from_num(39);
+  print_card(c); // 2c
+
+  c = card_from_num(51);
+  print_card(c); // Ac
+
+  /*
+  c = card_from_num(-1);
+  print_card(c); // assert
+
+  c = card_from_num(52);
+  print_card(c); //assert
+  */
+}
 
 int main(void) {
-
+  test_card_from_num();
+  return EXIT_SUCCESS;
 }
