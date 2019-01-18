a293ab527b87476c9ce63cbf92125ef9d9095b27
Coursera Learner <nobody@nowhere.nul>
2019-01-18 16:20:06 +0000

    wrote some functions

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -3,10 +3,40 @@
 #include <assert.h>
 #include "deck.h"
 void print_hand(deck_t * hand){
+  if (hand==NULL)
+    return;
 
+  card_t* c;
+  for (int i=0; i<hand->n_cards; ++i)
+    {
+      c = hand->cards[i];
+      assert(c);
+      print_card(*c);
+      printf(" ");
+    }
+}
+
+static int are_cards_same(card_t c1, card_t c2)
+{
+  if ( (c1.value==c2.value) && (c1.suit==c2.suit) )
+    return 1;
+  else
+    return 0;
 }
 
 int deck_contains(deck_t * d, card_t c) {
+  if (d==NULL)
+    return 0;
+
+  card_t* t;
+  for (int i=0; i<d->n_cards; ++i)
+    {
+      t = hand->cards[i];
+      assert(c);
+      if (are_cards_same(*t, c))
+	return 1;
+    }
+  
   return 0;
 }
 
